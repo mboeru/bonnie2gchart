710540a86ac470570b8e123e4ff35de3f6036968
mboeru <mboeru@gmail.com>
2013-11-04 19:43:50 +0200

    added all graphs to a single page

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 require_once 'inc/functions.php';
 include_once 'inc/labels.php';
 
-$file = 'bonnie.csv';
+if (isset($_GET['file'])) {
+	$file = $_GET['file'];
+} else {
+	$file = 'bonnie.csv';
+}
 $data = parse_bonnie_csv($file);
 
-if (!isset($_GET['t']) || empty($_GET['t'])) {
+/*if (!isset($ttype) || empty($ttype)) {
 	echo <<<EOT
 <!DOCTYPE html>
 <html>
@@ -27,45 +31,59 @@ EOT;
 </html>
 EOT;
 	exit;
-}
+}*/
+
+#$ttype = $_GET['t'];
+$ttypes = array_keys($types);
 
 echo <<<EOT
 <html>
   <head>
-	<title>bonnie2gchart - {$types[$_GET['t']]['name']}</title>
+	<title>bonnie2gchart - AAAA</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Tests');
+EOT;
+
+foreach ( $ttypes as $ttype ) {
+	echo <<<EOT
+	    <script type="text/javascript">
+	      google.load("visualization", "1", {packages:["corechart"]});
+	      google.setOnLoadCallback(drawChart);
+	      function drawChart() {
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Tests');
 
 EOT;
 
-foreach ($data['name'] as $val) {
-	printf("        data.addColumn('number', '%s')\n", $val);
+	foreach ($data['name'] as $val) {
+		printf("        data.addColumn('number', '%s')\n", $val);
+	}
+
+	foreach($types[$ttype]['types'] as $label)
+		echo addRow($data[$label], $labels[$label]);
+
+	echo <<<EOT
+		var options = {
+		  title: '{$types[$ttype]['title']}',
+		  vAxis: {title: '{$types[$ttype]['name']}',  titleTextStyle: {color: 'red'}}
+		};
+
+		var chart = new google.visualization.BarChart(document.getElementById('chart_div_{$ttype}'));
+		chart.draw(data, options);
+
+	      }
+	    </script>
+EOT;
 }
-
-foreach($types[$_GET['t']]['types'] as $label)
-	echo addRow($data[$label], $labels[$label]);
-
 echo <<<EOT
-        var options = {
-          title: '{$types[$_GET['t']]['title']}',
-          vAxis: {title: '{$types[$_GET['t']]['name']}',  titleTextStyle: {color: 'red'}}
-        };
-
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
   </head>
   <body>
-    <a href=".">« index</a>
-    <div id="chart_div" style="width: 900px; height: 500px;"></div>
-  </body>
-</html>
-
+    <a href=".">« index<a>
 EOT;
+foreach ( $ttypes as $ttype ) {
+	echo <<<EOT
+	    <div id="chart_div_{$ttype}" style="width: 900px; height: 500px;"></div>
+	  </body>
+	</html>
+EOT;
+}
